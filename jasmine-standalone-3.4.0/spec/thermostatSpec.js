a09e1fd8126653d295a298c89f7dd323f5fc8358
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-09-19 18:52:37 +0100

    Three tests failing with error 'isPowerSaving is not a function'.

--- jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -1,34 +1,28 @@
 'use strict'
 
-
-
-// (In the challenges where we add an interface, low-usage will
-// be indicated with green, medium-usage indicated with black, high-usage
-// indicated with red.)
-
-describe('Thermostat', function(){
+describe('Thermostat', function () {
   // Thermostat starts at 20 degrees
   var thermostat;
-  beforeEach(function(){
+  beforeEach(function () {
      thermostat = new Thermostat();
   });
 
-  it('should have a starting teperature at 20 degrees', function(){
-  expect(thermostat.temperature).toEqual(20)
+  it('should have a starting temperature at 20 degrees', function(){
+  expect(thermostat.getCurrentTemperature()).toEqual(20)
 });
   describe('up', function() {
         // You can increase the temperature with an up function
     it('can increase the temperature', function() {
       thermostat.up()
-      expect(thermostat.temperature).toBeGreaterThan(20)
+      expect(thermostat.getCurrentTemperature()).toBeGreaterThan(20)
     });
     // If power saving mode is off, the maximum temperature is 32 degrees
     it('it prevents temperature going above 32 if power mode is off', function(){
       thermostat.powerSavingOff()
-      for (var i = 0; i< 14 ;i++){
+      for (var i = 0; i < 14; i++){
         thermostat.up()
       }
-      expect(thermostat.temperature).toEqual(32)
+      expect(thermostat.getCurrentTemperature()).toEqual(32)
 
     })
   });
@@ -36,36 +30,47 @@ describe('Thermostat', function(){
     // You can decrease the temperature with a down function
     it('should reduce the temperature', function(){
       thermostat.down()
-      expect(thermostat.temperature).toBeLessThan(20)
+      expect(thermostat.getCurrentTemperature()).toBeLessThan(20)
     });
     it('should not reduce the temperature below 10', function() {
-      for ( var i = 11; i > 0 ; i-- ) {
+      for ( var i = 0; i < 12; i++ ) {
         thermostat.down()
       }
-      expect(thermostat.temperature).toEqual(10)
+      expect(thermostat.getCurrentTemperature()).toEqual(10)
     });
 
   });
   describe('power saving mode', function(){
     // Power saving mode is on by default
-    it('checks that power save mode is on by default', function(){
-      expect(thermostat.isPowerSaving).toBe(true);
-    })
+    it('sets power save mode to be on by default', function(){
+      console.log(thermostat.isPowerSaving())
+      expect(thermostat.isPowerSaving()).toBe(true);
+    });
+
+    // it('can be switched off', function() {
+    //   thermostat.powerSavingOff();
+    //   expect(thermostat.isPowerSaving()).toBe(false);
+    // });
+
+    // it('can be switched on', function() {
+    //   thermostat.powerSavingOff();
+    //   thermostat.powerSavingOn();
+    //   expect(thermostat.isPowerSaving()).toBe(true);
+    // });
     // If power saving mode is on, the maximum temperature is 25 degrees
     it('when power saving is on, temp cannot be increased above 25', function() {
       for (var i = 0; i < 6; i++) {
         thermostat.up()
       }
-      expect(thermostat.temperature).toEqual(25)
+      expect(thermostat.getCurrentTemperature()).toEqual(25)
     });
-
   })
   // You can reset the temperature to 20 with a reset function
   describe('reset', function() {
     it('can reset the temperature back to 20', function() {
       thermostat.up();
       thermostat.reset();
-      expect(thermostat.temperature).toEqual(20)
+      expect(thermostat.getCurrentTemperature()).toEqual(20)
     });
   });
     // You can ask about the thermostat's current energy usage: < 18 is low-usage,
@@ -76,7 +81,7 @@ describe('Thermostat', function(){
         thermostat.down()
       }
       expect(thermostat.checkEnergyUsage()).toEqual('Low')
-    })
+    });
     it('returns medium usage when temperature less than 25 but greater than 18', function(){
       expect(thermostat.checkEnergyUsage()).toEqual('Medium')
     });
@@ -85,8 +90,7 @@ describe('Thermostat', function(){
       for (var i = 0; i < 5; i++) {
         thermostat.up();
       }
-      console.log(thermostat.temperature)
       expect(thermostat.checkEnergyUsage()).toEqual('High')
     });
-  })
+  });
 });
--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -3,7 +3,16 @@
 
 function Thermostat (){
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10;
   this.isPowerSaving = true;
+}
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+}
+
+Thermostat.prototype.isPowerSaving = function() {
+  return this.isPowerSaving === true; // does this need to be set to true?
 }
 
 Thermostat.prototype.up = function(){
@@ -17,18 +26,30 @@ if (this.isPowerSaving===false){
   };
  };
 };
-Thermostat.prototype.down = function(){
-  if (this.temperature > 10) {
-    this.temperature --;
-  }
-  return this.temperature
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE
 }
-Thermostat.prototype.powerSavingOff = function(){
+
+Thermostat.prototype.down = function(){
+  if (this.isMinimumTemperature()) {
+    return
+  }
+  this.temperature --;
+}
+
+Thermostat.prototype.powerSavingOff = function() {
   this.isPowerSaving = false;
 }
+
+Thermostat.prototype.powerSavingOn = function() {
+  this.isPowerSaving = true;
+}
+
 Thermostat.prototype.reset = function() {
   this.temperature = 20
 }
+
 Thermostat.prototype.checkEnergyUsage = function(){
   if(this.temperature < 18){
     return 'Low';
